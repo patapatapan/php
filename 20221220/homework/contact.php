f569bb2a0c78f6ac7ea08dcd81752d825a9d52ca
patapatapan <johnny8947276@gmail.com>
2022-12-20 22:35:30 +0800

    回家作業:完成

--- 20221220/homework/contact.php
+++ 20221220/homework/contact.php
@@ -6,15 +6,20 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>與我聯絡</title>
+
+
+  <script>
+    document.getElementById('form').style.display = "none";
+  </script>
 </head>
 
 <body>
-  <form action="contact.php" method="post">
+  <form action="http://localhost:6080/20221220/homework/contact.php" id="form`" method="post">
     <h2>聯絡表單</h2>
     <p>姓名: </p>
-    <input type="text" name="姓名" value="姓名">
+    <input type="text" name="姓名" value="">
     <p>電話: </p>
-    <input type="number" name="電話" value="電話">
+    <input type="tel" name="電話" value="">
     <p>意見:</p>
     <textarea name="意見" cols="30" rows="5"></textarea>
 
@@ -26,3 +31,31 @@
 </body>
 
 </html>
+
+
+<?php
+
+if (empty($_POST['姓名']) || empty($_POST['電話']) || empty($_POST['意見'])) {
+  echo "請填表單";
+  $count = 0;
+} else {
+  echo "<script>
+  document.getElementById('form').style.display = 'none';
+  </script>";
+  $name = $_POST['姓名'];
+  $tel = $_POST['電話'];
+  $contact = $_POST['意見'];
+
+
+
+
+
+  echo "姓名: " . $name . "<br>";
+  echo "電話: " . $tel . "<br>";
+  echo "意見: " . $contact . "<br>";
+}
+
+
+
+
+?>
